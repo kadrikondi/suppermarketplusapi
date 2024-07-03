13b54d33ab5a283873c95c44196e365ad5bdd1fd
kadrikondi <abdulkadri42@gmail.com>
2024-07-03 14:25:14 +0100

    update suppermarket model

--- model/supermarketModel.js
+++ model/supermarketModel.js
@@ -2,6 +2,18 @@ import mongoose from "mongoose";
 
 const supermarketSchema = new mongoose.Schema({
   name: { type: String },
+  phone: { type: String },
+  email: { type: String },
+  street: { type: String },
+  city: { type: String },
+  building: { type: String },
+  state: { type: String },
+  whatsappno: { type: String },
+  images: [{ type: String }], // Array of image URLs
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "review" }], // Reference to the review model
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "user" }, // Reference to the user who created the supermarket
+  dateCreated: { type: Date, default: Date.now() },
+  verified: { type: Boolean, default: false },
 });
 
 const Supermarket = mongoose.model("Supermarket", supermarketSchema);
